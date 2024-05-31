862efab9981dfeddecf545508cb64025ce7e19be
meewaldor <tieumylam91@gmail.com>
2024-05-31 18:02:05 +0700

    Setting up pattern

---
+++ Repositories/Entities/Gold.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Repositories.Entities
+{
+    public class Gold
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(50)"), Required]
+        public string Name { get; set; }
+        [Column(TypeName = "nvarchar(50)"), Required]
+        public string Unit { get; set; }
+        [Column(TypeName = "decimal(18,2)"), Required]
+        public float Content { get; set; }
+
+    }
+}
