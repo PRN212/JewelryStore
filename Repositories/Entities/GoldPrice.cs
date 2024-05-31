410628ad885be013b57cd9576624fee9729fc8b8
meewaldor <tieumylam91@gmail.com>
2024-06-01 00:27:45 +0700

    Added three layer architecture

---
+++ Repositories/Entities/GoldPrice.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Repositories.Entities
+{
+    public class GoldPrice
+    {
+        public int Id { get; set; }
+        [Required]
+        public int GoldId { get; set; }
+        public Gold Gold {  get; set; }
+        [Required]
+        public DateTime DateTime { get; set; }
+        [Column(TypeName = "decimal(18,2)"), Required]
+        public float AskPrice { get; set; }
+        [Column(TypeName = "decimal(18,2)"), Required]
+        public float BidPrice { get; set; }
+        [Column(TypeName = "decimal(5,2)"), Required]
+        public float AskRate { get; set; }
+        [Column(TypeName = "decimal(5,2)"), Required]
+        public float BidRate { get; set;}
+    }
+}
---
+++ Repositories/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Repositories.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(200)"), Required]
+        public string Name { get; set; }
+        [Column(TypeName = "nvarchar(500)")]
+        public string Description { get; set; }
+        public int GoldId { get; set; }
+        public Gold Gold { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public float GoldWeight { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public float TotalWeight { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string GemName { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public float GemWeight { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public float GemPrice { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public float Labour {  get; set; }
+        public int Quantity { get; set; }
+        [Column(TypeName = "varchar(200)")]
+        public string ImgUrl { get; set; }
+
+    }
+}
